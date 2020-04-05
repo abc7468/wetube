15af0246e35c799b4e65cdf0821e4116971dc9bd
20170981 <abc7468@naver.com>
2020-04-06 02:47:43 +0900

    wetube middleware#2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+import express from "express"
+import morgan from "morgan"
+import helmet from "helmet"
+import cookieParser from "cookie-parser"
+import bodyParser from "body-parser"
+const app = express();
+const PORT = 4000;
+const handleListening = () => console.log(`Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from ass');
+
+const handleProfile = (req,res) => res.send("your on my profile");
+app.use(cookieParser());
+app.use(bodyParser());
+
+app.use(morgan("dev"));
+app.use(helmet());
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
