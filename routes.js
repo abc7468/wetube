04e144a9382c81134519e81298c191ae54c8070a
20170981 <abc7468@naver.com>
2020-04-12 05:07:43 +0900

    MVC #3

--- routes.js
+++ routes.js
@@ -9,7 +9,7 @@ const SEARCH = "/search";
 //users
 
 const USERS = "/users";
-const USER_DETAIL = "/:id/";
+const USER_DETAIL = "/:id";
 const EDIT_PROFILE = "/edit-profile";
 const CHANGE_PASSWORD = "/change-password";
 
